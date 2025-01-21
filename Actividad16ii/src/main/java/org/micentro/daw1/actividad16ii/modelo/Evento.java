c64d48350e7cb58338d360424c49156a91efbdd0
Derlyn Romero <derlyndelvalle.romero.davila@zabalburu.org>
2025-01-21 13:54:40 +0100

    Actividad en Proceso

---
+++ Actividad16ii/src/main/java/org/micentro/daw1/actividad16ii/modelo/Evento.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.micentro.daw1.actividad16ii.modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author DAW1
+ */
+public class Evento {
+    private String descripcion;
+    private int id;
+    private Date fecha;
+    private String lugar;
+    private boolean mayorEdad;
+    private double coste;
+}
---
+++ Actividad16ii/src/main/java/org/micentro/daw1/actividad16ii/modelo/Persona.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.micentro.daw1.actividad16ii.modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author DAW1
+ */
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private int dni;
+    private Date fechaNacimiento;
+    private String password;
+    
+    
+    public boolean isMayorEdad(){
+        boolean isMayor = true;
+        return false;
+    }
+    
+}
